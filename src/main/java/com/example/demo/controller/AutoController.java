84d71fc4c54b54137228e7d5121194a146c4b323
JCarrasco1705 <98120346+JCarrasco1705@users.noreply.github.com>
2023-03-27 16:21:43 -0600

    se sube path status
    se sube path status

--- src/main/java/com/example/demo/controller/AutoController.java
+++ src/main/java/com/example/demo/controller/AutoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("auto")
 public class AutoController {
@@ -31,5 +33,12 @@ public class AutoController {
     public void deleteById(@PathVariable(name = "id") String id){
         service.deleteById(id);
     }
+
+    @GetMapping("/status")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Object> status() {
+        return Map.of("status", "UP");
+    }
+
 
 }
